3b3016d2e6898cc7c6869c2c02c86ec90f727fd3
Gustavo Huerta <gustavo_mybrother@hotmail.com>
2017-06-27 21:10:10 -0400

    Este proyecto ya esta terminado

---
+++ src/progressbar/ProgressBar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progressbar;
+
+/**
+ *
+ * @author root
+ */
+public class ProgressBar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ProgressBarDemo progressBarDemo = new ProgressBarDemo();
+        progressBarDemo.setVisible(true);
+    }
+    
+}
